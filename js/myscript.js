8b02ef297711913fe27c23bc087e232667d9be9d
MarioSantoro <mariosantoro124@gmail.com>
2023-05-16 15:42:50 +0200

    Forse è giusto o forse no

--- js/myscript.js
+++ js/myscript.js
@@ -1,11 +1,21 @@
+const container = document.getElementById("container");
+let p = document.createElement("p");
+
 for(let i=1;i<=100;i++){
     if(((i % 3)== 0) && ((i % 5)== 0)){
         console.log("FizzBuzz");
+        p.append("FizzBuzz")
     }else if((i % 5)== 0){
         console.log("Buzz");
+        p.append("Buzz")
     }else if((i % 3)== 0){
         console.log("Fizz");
+        p.append("Fizz")
     }else{
         console.log(i);
+        p.append(i)
     }
 }
+
+container.appendChild(p)
+

b1cda85be00b3508cb1a4106d88bbb801c553e92
MarioSantoro <mariosantoro124@gmail.com>
2023-05-16 16:02:01 +0200

    Fix general

--- js/myscript.js
+++ js/myscript.js
@@ -1,21 +1,33 @@
 const container = document.getElementById("container");
-let p = document.createElement("p");
+const containerTwo = document.getElementById("containerTwo");
+const containerThree = document.getElementById("containerThree");
+const containerFour = document.getElementById("containerFour");
 
 for(let i=1;i<=100;i++){
+    let li = document.createElement("li");
     if(((i % 3)== 0) && ((i % 5)== 0)){
         console.log("FizzBuzz");
-        p.append("FizzBuzz")
+        li.append("FizzBuzz")
     }else if((i % 5)== 0){
         console.log("Buzz");
-        p.append("Buzz")
+        li.append("Buzz")
     }else if((i % 3)== 0){
         console.log("Fizz");
-        p.append("Fizz")
+        li.append("Fizz")
     }else{
         console.log(i);
-        p.append(i)
+        li.append(i)
+    }
+    if(i<= 25){
+        container.appendChild(li);
+    }else if(i<= 50){
+        containerTwo.appendChild(li);
+    }else if(i<= 75){
+        containerThree.appendChild(li);
+    }else{
+        containerFour.appendChild(li);
     }
 }
 
-container.appendChild(p)
+
 

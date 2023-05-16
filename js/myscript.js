29e41e08ba862bd16a640db17a306ceff1d8b0a6
MarioSantoro <mariosantoro124@gmail.com>
2023-05-16 16:16:13 +0200

    Finish the second bonus

--- js/myscript.js
+++ js/myscript.js
@@ -12,15 +12,19 @@ for(let i=1;i<=100;i++){
     let li = document.createElement("li");
     if(((i % 3)== 0) && ((i % 5)== 0)){
         console.log("FizzBuzz");
+        li.classList.add("text-primary")
         li.append("FizzBuzz")
     }else if((i % 5)== 0){
         console.log("Buzz");
+        li.classList.add("text-danger")
         li.append("Buzz")
     }else if((i % 3)== 0){
         console.log("Fizz");
+        li.classList.add("text-success")
         li.append("Fizz")
     }else{
         console.log(i);
+        li.classList.add("text-warning")
         li.append(i)
     }
     if(i<= 25){
